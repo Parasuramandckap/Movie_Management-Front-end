c9586aef1ceeb207527a36bf8fec64c9f9c1ea84
Ganeshdckap <ganeshmdckap@gmail.com>
2023-10-30 10:10:18 +0530

    read more completed

--- src/components/home/Curosel.jsx
+++ src/components/home/Curosel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Carousel } from "antd";
 import loki from "../../assets/images/loki.png";
 const contentStyle = {
@@ -9,10 +9,17 @@ const contentStyle = {
   background: "#000000",
 };
 const Curosel = ({ movieList }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleReadMore = () => {
+    setIsExpanded(!isExpanded);
+  };
+  
   return (
     <div className="curosel">
       <Carousel effect="fade" autoplay>
         {movieList.map((movie, index) => {
+          const textShow = isExpanded ? movie.description : movie.description.slice(0,200)
           return (
             <div className="carousel-slide" style={contentStyle} key={index}>
               <img src={loki} />
@@ -32,7 +39,12 @@ const Curosel = ({ movieList }) => {
                   {/* <p className="like"><i class="fa-solid fa-thumbs-up"></i>500k likes</p>
                     <p className="unlike"><i class="fa-regular fa-thumbs-down"></i>500k unlikes</p> */}
                 </div>
-                <p className="curosel-description">{movie.description}</p>
+                <p className="curosel-description">{textShow}</p>
+                {movie.description.length > 200 && (
+                  <a onClick={toggleReadMore}>
+                    {isExpanded ? "Read Less" : "Read More"}
+                  </a>
+                )}
               </div>
             </div>
           );
